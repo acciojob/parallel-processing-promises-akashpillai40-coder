7643eebef995a50916128dcb562544c354ddf7f4
Akash <akashpillai40@gmail.com>
2026-05-02 21:21:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,47 +1,38 @@
-document.addEventListener("DOMContentLoaded", () => {
+const output = document.getElementById("output");
+const btn = document.getElementById("download-images-button");
 
-  const output = document.getElementById("output");
-  const loading = document.getElementById("loading");
-  const errorDiv = document.getElementById("error");
-  const btn = document.getElementById("download-images-button");
+const images = [
+  { url: "https://picsum.photos/id/237/200/300" },
+  { url: "https://picsum.photos/id/238/200/300" },
+  { url: "https://picsum.photos/id/239/200/300" },
+];
 
-  const images = [
-    { url: "https://picsum.photos/id/237/200/300" },
-    { url: "https://picsum.photos/id/238/200/300" },
-    { url: "https://picsum.photos/id/239/200/300" },
-  ];
+// download single image
+function downloadImage(url) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.src = url;
 
-  function downloadImage(url) {
-    return new Promise((resolve, reject) => {
-      const img = new Image();
-      img.src = url;
+    img.onload = () => resolve(img);
+    img.onerror = () => reject("Error loading " + url);
+  });
+}
 
-      img.onload = () => resolve(img);
-      img.onerror = () => reject("Failed: " + url);
-    });
-  }
+// main function
+function downloadImages() {
+  output.innerHTML = "";
 
-  function downloadImages() {
-    output.innerHTML = "";
-    errorDiv.textContent = "";
-    loading.style.display = "block";
+  const promises = images.map((img) => downloadImage(img.url));
 
-    const promises = images.map((img) => downloadImage(img.url));
-
-    Promise.all(promises)
-      .then((results) => {
-        loading.style.display = "none";
-
-        results.forEach((img) => {
-          output.appendChild(img);
-        });
-      })
-      .catch((err) => {
-        loading.style.display = "none";
-        errorDiv.textContent = err;
+  Promise.all(promises)
+    .then((results) => {
+      results.forEach((img) => {
+        output.appendChild(img);
       });
-  }
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
 
-  btn.addEventListener("click", downloadImages);
-
-});
+btn.addEventListener("click", downloadImages);
